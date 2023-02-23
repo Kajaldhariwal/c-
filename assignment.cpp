8cafcfa48959eeb7e29770306a87aea5f6536642
Kajaldhariwal <124187927+Kajaldhariwal@users.noreply.github.com>
2023-02-23 11:34:49 -0500

    Add files via upload

---
+++ assignment.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter first integer"<<endl;
+	cin>>a;
+	
+	int b;
+	cout<<"enter second integer"<<endl;
+	cin>>b;
+	
+	if (a>b){
+	
+	cout<<"greater is"<<  a  <<endl;
+}
+	else{
+	
+	cout<<"greater is"<<   b<<endl;
+}
+}
